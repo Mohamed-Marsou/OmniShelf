e55c453138d7029148a65c4527d8b1bfa2f15b42
MTM <marsoumtm@gmail.com>
2024-07-18 16:47:25 +0100

    init push

---
+++ Omni-react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ fontSize: '48px', textAlign: 'center', marginTop: '20%' }}>OmniShelf React app</h1>
+    </>
+  );
+}
+
+export default App;
