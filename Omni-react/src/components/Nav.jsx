ed90a7386a903da30f80db00172f0794f3107c19
MTM <marsoumtm@gmail.com>
2024-07-26 12:39:32 +0100

    static nav

--- Omni-react/src/components/Nav.jsx
+++ Omni-react/src/components/Nav.jsx
@@ -2,9 +2,49 @@ import Link from 'react-dom'
 import "../scss/nav.scss"
 function Nav() {
 
-    return(
+    return (
         <nav>
-           <h1>Nav</h1>
+            <div className="form_wrapper">
+
+                <form className='search_inp'>
+                    <input type="search" placeholder='SEARCH' />
+                    <button type='submit'>
+                        <img src="/icons/search.png" alt="search" />
+                    </button>
+                </form>
+            </div>
+
+            <div className="profile_wrapper">
+
+                <div className="level_box" title='30%'>
+
+                    <div className="level-progress"></div>
+                    <img src="/icons/level-icons/l1.png" alt="level-icon" title='Level 1' />
+                </div>
+
+                <div className="user">
+                    <div className="notification">
+                        <div className='n-w'>
+                            <img src="/icons/notification-icon.png" alt="notification-icon" title='Notification'/>
+                            <div className="n-counter">
+                                01
+                            </div>
+                        </div>
+                    </div>
+                    <div className="user-profile">
+                        <div>
+                            <img src="/icons/user-icon.png" alt="user-icon" title='Visit Profile' />
+                        </div>
+                    </div>
+                    <div className="mobile-search-btn">
+                        <div>
+                            <img src="/icons/search.png" alt="search" title='Search' />
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </nav>
     )
 }
